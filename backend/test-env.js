1b6f2408bac2ed406ad8bea0a95dad99616f42a2
vonzii1 <your-email@example.com>
2025-03-24 00:01:39 +0800

    Updated various files and added new features

---
+++ backend/test-env.js
@@ -0,0 +1,2 @@
+require('dotenv').config();
+console.log("S3_BUCKET:", process.env.S3_BUCKET);
---
+++ backend/test-load-env.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+console.log("AWS_ACCESS_KEY:", process.env.AWS_ACCESS_KEY);
+console.log("AWS_SECRET_KEY:", process.env.AWS_SECRET_KEY);
+console.log("AWS_REGION:", process.env.AWS_REGION);
+console.log("S3_BUCKET:", process.env.S3_BUCKET);
