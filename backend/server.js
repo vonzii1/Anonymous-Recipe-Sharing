7e16c793ccba9c165f1f458a14fa058b51fd4476
vonzii1 <your-email@example.com>
2025-04-09 00:44:46 +0800

    Updated callback URL for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.use(helmet({
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors({
-    origin: ['http://127.0.0.1:5000', 'http://localhost:5000', 'http://localhost:5500'], // Add your frontend origin
+    origin: ['https://anonymous-recipe-sharing-production-1948.up.railway.app/auth/google/callback', 'http://localhost:5000', 'http://localhost:5500'], // Add your frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow POST and OPTIONS
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
     credentials: true
